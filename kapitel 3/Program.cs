31563977984ae78a5c81497f3e1d71bb0548b37d
Muhammad Abdullah <04MUABB@skola.lidkoping.se>
2021-09-07 09:12:34 +0200

    uppgift 3.3

--- kapitel 3/Program.cs
+++ kapitel 3/Program.cs
@@ -6,7 +6,28 @@ namespace kapitel_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+
+            Console.WriteLine("vad heter du?");
+            string namn = Console.ReadLine().ToLower();
+
+            if (namn == "stig")
+            {
+                Console.WriteLine("du har namnsdag ideg");
+            }
+
+            else if (namn == "abdraham")
+            {
+                Console.WriteLine("du har namnsdag imorgon");
+            }
+
+            if 
+            
+            
+            
+           
+            
+
+
         }
     }
 }
