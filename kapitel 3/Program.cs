42f5f116d067458008f6ad8189b2dfa902639ce4
Muhammad Abdullah <04MUABB@skola.lidkoping.se>
2021-09-07 08:19:31 +0200

    Add project files.

---
+++ kapitel 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kapitel_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
